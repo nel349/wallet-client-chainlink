2f840ad994231ead0873f7cbe7c9b6c1ae7773b5
Norman Lopez <noell.lpz@gmail.com>
2023-06-08 00:54:16 +0200

    Fixed the UI of the button and handles execution types rendering.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { ExecutionButtons, ExecutionType } from './ExecutionButtons';
 
-const HeroSection: React.FC = () => (
+type Props = {
+  executionType: string | ExecutionType;
+  otherParameter: string | null;
+};
+
+const HeroSection: React.FC<Props> = ({ executionType, otherParameter }: Props) => (
   <section className="hero">
     <div className="container">
       <h1 className="hero-title">Welcome to Chainlink</h1>
       <p className="hero-description">Decentralized oracle network</p>
-      <ExecutionButtons executionType={ExecutionType.CreateSubscriptionCall}/>
+      <ExecutionButtons executionType={executionType}/>
     </div>
   </section>
 );
--- index.tsx
+++ index.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'viem/window'
-import { ExecutionButtons, ExecutionType } from './components/ExecutionButtons';
 import HeroSection from './components/HeroSection';
+import { ExecutionType } from './components/ExecutionButtons';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
+const searchParams = new URLSearchParams(window.location.search);
+const executionTypeParam = searchParams.get('executionType') ?? "";
+const otherParameter = searchParams.get('otherParameter');
+console.log("executionTypeParam: ", executionTypeParam);
+
 root.render(
-  <HeroSection />
+  <HeroSection executionType={executionTypeParam} otherParameter={otherParameter} />
 )
+
+// function toPascalCase(str: string): string {
+//   return str.replace(/(\w)(\w*)/g, (_, firstChar, rest) => firstChar.toUpperCase() + rest.toLowerCase());
+// }
